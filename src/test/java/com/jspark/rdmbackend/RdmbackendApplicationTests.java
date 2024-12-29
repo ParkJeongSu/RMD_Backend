63f489a00805b89f96ca7f18988be0bc8855174e
ParkJeongSu <pjs91_marrow@naver.com>
2024-12-29 15:07:39 +0900

    rdm init set

---
+++ src/test/java/com/jspark/rdmbackend/RdmbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspark.rdmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdmbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
