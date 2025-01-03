bb1fd1fdfb28e4cf1f27fedc2719aeccd2688e07
ParkJeongSu <pjs91_marrow@naver.com>
2025-01-03 22:36:49 +0900

    State 테스트

--- src/main/java/com/jspark/rdmbackend/controller/StateController.java
+++ src/main/java/com/jspark/rdmbackend/controller/StateController.java
@@ -28,6 +28,55 @@ public class StateController {
         return ResponseEntity.ok(stateDto);
     }
 
+    @PostMapping("/test")
+    public ResponseEntity<StateDto> testState(@RequestBody StateDto stateDto) {
+
+        boolean A1machine3 = false;
+        boolean A1machine6 = false;
+        boolean T1machine1 = false;
+        for(int i=0;i<1000;i++)
+        {
+            try {
+                Thread.sleep(200); // 2000 밀리초 = 2초
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("2초 후에 다시 실행됩니다.");
+            StateDto test= new StateDto();
+            if(i%2==0)
+            {
+                test.setObjectName("A1machine3");
+                test.setStateName("MachineState");
+                test.setStateValue(A1machine3 == true ? "Run" : "Down");
+                A1machine3 = !A1machine3;
+            }
+            else if(i%3==0)
+            {
+                test.setObjectName("A1machine6");
+                test.setStateName("MachineState");
+                test.setStateValue(A1machine6 == true ? "Run" : "Down");
+                A1machine6 = !A1machine6;
+            }
+            else if(i%5==0)
+            {
+                test.setObjectName("T1machine1");
+                test.setStateName("MachineState");
+                test.setStateValue(T1machine1 == true ? "Run" : "Down");
+                T1machine1 = !T1machine1;
+            }
+            else
+            {
+                test.setObjectName("T1machine1");
+                test.setStateName("MachineState");
+                test.setStateValue(T1machine1 == true ? "Run" : "Down");
+                T1machine1 = !T1machine1;
+            }
+            messagingTemplate.convertAndSend("/topic/state",test);
+        }
+        return ResponseEntity.ok(stateDto);
+    }
+
+
     @GetMapping
     public List<StateDto> getAllState(){
         return stateService.getAllState();
--- src/main/java/com/jspark/rdmbackend/dto/StateDto.java
+++ src/main/java/com/jspark/rdmbackend/dto/StateDto.java
@@ -2,14 +2,14 @@ package com.jspark.rdmbackend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class StateDto {
     private String objectName;
     private String stateName;
     private String stateValue;
-
-
 }
