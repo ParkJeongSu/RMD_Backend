3eaff661b68c0fe90e264fe127d87a01f91c4b0b
ParkJeongSu <pjs91_marrow@naver.com>
2025-01-10 16:46:52 +0800

    add Log & config

--- src/main/java/com/jspark/rdmbackend/service/FileStorageService.java
+++ src/main/java/com/jspark/rdmbackend/service/FileStorageService.java
@@ -1,4 +1,6 @@
 package com.jspark.rdmbackend.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,11 +15,19 @@ public class FileStorageService {
     @Value("${file.upload.dir}")
     private String uploadDir;
 
+    private static final Logger logger = LoggerFactory.getLogger(FileStorageService.class);
+
     public String storeFile(MultipartFile file) throws IOException {
+
+        logger.info("info");
+        logger.debug("debug");
+        logger.warn("warn");
+        logger.error("error");
 
         if (!file.getOriginalFilename().endsWith(".svg") ) {
             throw new RuntimeException("Only SVG files are allowed.");
         }
+
 
         // 저장 경로 생성 (존재하지 않으면 생성)
         Path uploadPath = Paths.get(uploadDir);
